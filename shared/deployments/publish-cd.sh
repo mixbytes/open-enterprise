184f14ca7b7c6e92085690f946f74b53eda620a9
Kevin Siegler <kevinsiegler54@gmail.com>
2019-10-30 16:34:53 -0500

    [dev] test updated publish flow

--- shared/deployments/publish-cd.sh
+++ shared/deployments/publish-cd.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 IFS=$'\n'
-APP_CIDs=($(lerna run publish:cd --no-ci | grep -o "Qm[[:alnum:]]\{44\}$"))
+#APP_CIDs=($(lerna run publish:cd --no-ci --stream| grep -o "Qm[[:alnum:]]\{44\}$"))
+lerna run publish:cd --no-ci --stream | tee publishLog.txt
+APP_CIDs=($(grep -o "Qm[[:alnum:]]\{44\}$" publishLog.txt))
 unset IFS
 echo number of apps published: ${#APP_CIDs[*]}
 
 for cid in "${APP_CIDs[@]}"
 do
+  echo pinning $cid...
+  curl "https://ipfs.autark.xyz:5001/api/v0/pin/add?arg=$cid&progress=true"
   echo propagating $cid ... $'\n'
-  aragon ipfs propagate $cid > /dev/null &
-  sleep 20
+  aragon ipfs propagate $cid
 done
